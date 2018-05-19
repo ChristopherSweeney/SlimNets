619bf6947213544157488fc3355509cafe478ad9
Subby Olubeko <subbyo@mit.edu>
2018-05-18 21:01:47 -0400

    One more update

--- Low Rank Factorization/test-lowrank.py
+++ Low Rank Factorization/test-lowrank.py
@@ -121,6 +121,7 @@ if __name__ == '__main__':
         os.makedirs(args['save_dir'])
     model_path = os.path.join(args['save_dir'], 'lowrank-model-best.tar')
     lrm = load_lowrank_model(model_path)
+    normalize = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     testset = datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transforms.Compose([
             transforms.ToTensor(),
